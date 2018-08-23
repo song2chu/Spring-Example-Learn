323423a798e4e64c728429a319bfc6ac3251ef6c
tod2chu <42412641+tod2chu@users.noreply.github.com>
2018-08-23 16:43:07 -0300

    spring_example_part_5
    XML Setter Method    
    ใช้ spring ฉีด ให้มันรู้จักกัน

---
+++ spring_example_part_5/src/java/service/CustomerServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.List;
+import model.Customer;
+import repository.CustomerRepository;
+import repository.HibernateCustomerRepositoryImpl;
+
+/**
+ *
+ * @author safev
+ */
+public class CustomerServiceImpl implements CustomerService{
+//    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+    
+}
